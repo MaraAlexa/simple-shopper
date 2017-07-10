2a074776b21e4a96f428c475c9781bbabd92debf
maraalexa <maraalexa57@gmail.com>
2017-07-10 12:18:02 +0200

    added working tabs items and cart

--- src/App.js
+++ src/App.js
@@ -7,12 +7,35 @@ import Nav from './components/Nav'
 import products from './data/products'
 
 class App extends Component {
+  state = {
+    activeTab: 0
+  }
+
+  handleTabChange = (index) => {
+    this.setState({
+      activeTab: index
+    })
+  }
+
+  renderContent() {
+    switch (this.state.activeTab) {
+      default:
+      case 0: return <span>Items</span>
+      case 1: return <span>Cart</span>
+    }
+  }
+
   render() {
+    let {activeTab} = this.state
     return (
       <div className="App">
-        <Nav/>
+        <Nav
+          activeTab={activeTab}
+          onTabChange={this.handleTabChange}
+        />
         <div className="content">
-          <ItemsPage products={products} />
+          {this.renderContent()}
+          {/* <ItemsPage products={products} /> */}
         </div>
 
       </div>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import '../styles/Nav.css'
 
-const Nav = () =>
+const Nav = ({ activeTab, onTabChange }) =>
 
 <nav className="navbar">
   <div className="navbar-brand">
@@ -10,10 +10,15 @@ const Nav = () =>
       <img src="http://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28" />
     </a>
   </div>{/* end navbar-brand */}
-  <div className="navbar-end">
-    <a href="" className="navbar-item">ItemsPage</a>
-    <a href="" className="navbar-item">Cart</a>
-  </div>
+  <ul className="navbar-end">
+    <li className={`nav-item ${activeTab === 0 && 'selected'}`}>
+      <a onClick={() => onTabChange(0)} className="navbar-item">ItemsPage</a>
+    </li>
+    <li className={`nav-item ${activeTab === 1 && 'selected'}`}>
+      <a onClick={() => onTabChange(1)} className="navbar-item">Cart</a>
+    </li>
+
+  </ul>
 
   <div className="navbar-burger">
     <span></span>
