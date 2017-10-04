c2d33720d24c7c0cd9460e28374f851701ddff04
maraalexa <maraalexa57@gmail.com>
2017-10-04 11:25:17 +0200

    working version

--- src/components/static/LandingPageStatic.js
+++ src/components/static/LandingPageStatic.js
@@ -1,28 +1,32 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import '../../styles/LandingPageStatic.css'
+import ReactCSSTransitionGroup from 'react-transition-group'
 
-
-const LandingPageStatic = () =>
+const LandingPageStatic = () => (
   <div className="landing-page">
     <div className="page-header">
-      <div className="filter"></div>
+      <div className="filter" />
       <div className="content-center">
         <p className="title is-size-1">An open road adventure</p>
         <p className="subtitle is-size-4">Made possible by our reliable gear</p>
 
         <NavLink
-          activeClassName='active'
+          activeClassName="active"
           className="button is-success cta-large-btn"
-          to='/products'
-          >
-            See our products
+          to="/products"
+        >
+          See our products
         </NavLink>
+        <br />
       </div>
     </div>
+  </div>
 
-    {/* <p className="title best-selling-title">Best selling products</p> */}
-    {/* <div className="best-seller-wrap columns">
+  /* <p className="title best-selling-title">Best selling products</p> */
+
+
+  /* <div className="best-seller-wrap columns">
       <div className="card column">
         <div className="card-image">
           <figure className="image is-3by2 static-product-one">
@@ -44,10 +48,8 @@ const LandingPageStatic = () =>
           </figure>
         </div>
       </div>
-    </div> */}
-  </div>
-
-
+    </div> */
+)
 
 
 export default LandingPageStatic
